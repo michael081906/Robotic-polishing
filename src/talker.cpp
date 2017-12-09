b2c963698ec6b0d591d9c4fc624dc381aaaf17c6
Michael <michael081906@gmail.com>
2017-12-08 21:08:53 -0500

    Iteration #2 Task #2: Add tf in talkerpcl node to transform point cloud

--- src/talker.cpp
+++ src/talker.cpp
@@ -45,7 +45,6 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 
-
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 PointCloud pointcloud_in;
@@ -55,10 +54,15 @@ void callback(const sensor_msgs::PointCloud2Ptr& cloud) {
 
   pcl::fromROSMsg(*cloud, pointcloud_in);  // copy sensor_msg::Pointcloud message into pcl::PointCloud
 
-  tf::Transform transformKinect;
-  transformKinect.setOrigin(tf::Vector3(0.0, 0.0, 1.0));
-  tf::Quaternion q;
-  transformKinect.setRotation(tf::Quaternion(0, 0, 0, 1));
+  // tf::Transform transformKinect;
+  tf::Pose transformKinect;
+  transformKinect.setOrigin(tf::Vector3(1.0, 0.0, 0.0));
+  tf::Matrix3x3 rotationMatrix(0, 0, 1, -1, 0, 0, 0, -1, 0);
+ // tf::Matrix3x3 rotationMatrix(1, 0, 0, 0, 1, 0, 0, 0, 1);
+  transformKinect.setBasis(rotationMatrix);
+  // tf::Quaternion q;
+  // TODO: (Michael) What is Quaternion;
+  // transformKinect.setRotation(tf::Quaternion(0, 0, 0, 1));
   pcl_ros::transformPointCloud(pointcloud_in, pointcloud_out, transformKinect);
 }
 
@@ -91,157 +95,156 @@ bool find(robotic_polishing::Trajectory::Request &req,
 
   //  1. Load pcd file
   /*std::cout << " 1. Loading pcd file, please wait..." << std::endl;
-  pclLoad.readPCDfile("./src/Robotic-polishing/kidney3dots3_p1.pcd");
-  pclLoad.getPointCloud(*cloud_out);
-  std::cout << " Loading completed" << std::endl;
+   pclLoad.readPCDfile("./src/Robotic-polishing/kidney3dots3_p1.pcd");
+   pclLoad.getPointCloud(*cloud_out);
+   std::cout << " Loading completed" << std::endl;
 
 
 
-  //  2. Remove the noise of point cloud
-  std::cout << " 2. Removing the noise of point cloud, please wait..."
-            << std::endl;
-  sor.setInputCloud(*cloud_out);
-  sor.setMeanK(50);
-  sor.setStddevMulThresh(1);
-  sor.filterProcess(*cloud_out);
-  //*******************************************************************
-  // 2017.11.9 Added Michael
-  pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
-  // build the filter
-  outrem.setInputCloud(cloud_out);
-  outrem.setRadiusSearch(3);
-  outrem.setMinNeighborsInRadius(100);
-  // apply filter
-  outrem.filter(*cloud_out);
-  //*******************************************************************
-  std::cout << " Removing completed" << std::endl;
-  /*****************************************************************/
+   //  2. Remove the noise of point cloud
+   std::cout << " 2. Removing the noise of point cloud, please wait..."
+   << std::endl;
+   sor.setInputCloud(*cloud_out);
+   sor.setMeanK(50);
+   sor.setStddevMulThresh(1);
+   sor.filterProcess(*cloud_out);
+   //*******************************************************************
+   // 2017.11.9 Added Michael
+   pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
+   // build the filter
+   outrem.setInputCloud(cloud_out);
+   outrem.setRadiusSearch(3);
+   outrem.setMinNeighborsInRadius(100);
+   // apply filter
+   outrem.filter(*cloud_out);
+   //*******************************************************************
+   std::cout << " Removing completed" << std::endl;
+   /*****************************************************************/
   //  4. Down sample the point cloud
   /*std::cout << " 4. Down sampling the point cloud, please wait..." << std::endl;
-  vx.setInputCloud(*cloud_out);
-  vx.setLeafSize(0.2, 0.2, 0.2);
-  vx.filterProcess(*cloud_out);
-  std::cout << " Down sampling completed" << std::endl;
-  //  3. Extract certain region of point cloud
-  std::cout << " 3. Extracting certain region of point cloud, please wait..."
-            << std::endl;
-  pt.setInputCloud(*cloud_out);
-  pt.setFilterXlimit(4.5, 23.0);
-  pt.filterProcess(*cloud_out);
-  pt.setInputCloud(*cloud_out);
-  pt.setFilterYlimit(5.0, 16.5);
-  pt.filterProcess(*cloud_out);
-  pt.setInputCloud(*cloud_out);
-  pt.setFilterZlimit(7, 11.0);
-  pt.filterProcess(*cloud_out);
-  std::cout << " Extracting completed" << std::endl;
-  //  5. Smooth the point cloud
-  std::cout << " 5. Smoothing the point cloud, please wait..." << std::endl;
-  mls.setInputCloud(*cloud_out);
-  mls.setSearchRadius(4);
-  mls.mlsProcess(*cloud_with_normal);
-  std::cout << " Smoothing completed" << std::endl;
-  //  7. Mesh the obstacle
-  std::cout << " 7. Meshing the obstacle, please wait..." << std::endl;
-  ft.setInputCloud(*cloud_with_normal);
-  ft.setSearchRadius(4);
-  ft.reconctruct(triangles);
-  std::vector<int> gp33 = ft.getSegID();
-  int sizegp3 = gp33.size();
-  int sizePointCloud = cloud_with_normal->size();
-  std::cout << " Meshing completed..." << std::endl;
-  std::cout << " size of gp3: " << sizegp3 << std::endl;
-  //  8. Show the result
-  //pclView.display(triangles);
-  // 9.mergeHanhsPoint
-  std::cout << " 9. Merging point of selected point, please wait..."
-            << std::endl;
-  findNearestPoint mg;
-  std::vector<int> selectedPoint;
+   vx.setInputCloud(*cloud_out);
+   vx.setLeafSize(0.2, 0.2, 0.2);
+   vx.filterProcess(*cloud_out);
+   std::cout << " Down sampling completed" << std::endl;
+   //  3. Extract certain region of point cloud
+   std::cout << " 3. Extracting certain region of point cloud, please wait..."
+   << std::endl;
+   pt.setInputCloud(*cloud_out);
+   pt.setFilterXlimit(4.5, 23.0);
+   pt.filterProcess(*cloud_out);
+   pt.setInputCloud(*cloud_out);
+   pt.setFilterYlimit(5.0, 16.5);
+   pt.filterProcess(*cloud_out);
+   pt.setInputCloud(*cloud_out);
+   pt.setFilterZlimit(7, 11.0);
+   pt.filterProcess(*cloud_out);
+   std::cout << " Extracting completed" << std::endl;
+   //  5. Smooth the point cloud
+   std::cout << " 5. Smoothing the point cloud, please wait..." << std::endl;
+   mls.setInputCloud(*cloud_out);
+   mls.setSearchRadius(4);
+   mls.mlsProcess(*cloud_with_normal);
+   std::cout << " Smoothing completed" << std::endl;
+   //  7. Mesh the obstacle
+   std::cout << " 7. Meshing the obstacle, please wait..." << std::endl;
+   ft.setInputCloud(*cloud_with_normal);
+   ft.setSearchRadius(4);
+   ft.reconctruct(triangles);
+   std::vector<int> gp33 = ft.getSegID();
+   int sizegp3 = gp33.size();
+   int sizePointCloud = cloud_with_normal->size();
+   std::cout << " Meshing completed..." << std::endl;
+   std::cout << " size of gp3: " << sizegp3 << std::endl;
+   //  8. Show the result
+   //pclView.display(triangles);
+   // 9.mergeHanhsPoint
+   std::cout << " 9. Merging point of selected point, please wait..."
+   << std::endl;
+   findNearestPoint mg;
+   std::vector<int> selectedPoint;
 
-  //mg.readtext("./src/Robotic-polishing/kidney3dots3_pointCluster.txt");
-  mg.setPosition(req.start);
-  mg.setPosition(req.end);
+   //mg.readtext("./src/Robotic-polishing/kidney3dots3_pointCluster.txt");
+   mg.setPosition(req.start);
+   mg.setPosition(req.end);
 
-  mg.setInputCloud(*cloud_with_normal);
-  mg.findNearestProcess(selectedPoint);
-  std::cout << " 9. Merging completed" << std::endl;
-  std::vector<int> size3;
-  size3 = ft.getSegID();
-  std::cout << selectedPoint[0] << " " << selectedPoint[1] << " "
-            << size3.size() << std::endl;
-  std::cout << " 10. Delaunay3 function..." << std::endl;
-  std::vector<Triad> triads;
-  std::vector<Shx> ptsOut;
-  delaunay3 dy3;
-  dy3.setInputCloud(*cloud_with_normal);
-  dy3.putPointCloudIntoShx();
-  dy3.processDelaunay(triads);
-  dy3.getShx(ptsOut);
-  // write_Triads(triads, "triangles.txt");
-  // write_Shx(ptsOut, "pts.txt");
-  std::cout << " 10. Delaunay3 function completed..." << std::endl;
-  int start = selectedPoint[0];
-  int end = selectedPoint[1];
-  std::vector<int> path;
-  dijkstraPQ dPQ(size3.size());
-  dPQ.setInputCloud(*cloud_with_normal);
-  dPQ.setTri(triads);
-  dPQ.computeWeight();
-  dPQ.shortestPath(start, end);
-  dPQ.returnDijkstraPath(start, end, path);
-  res.path = path;
-  /*
-  std::vector<int>::iterator route = path.begin();
-  while (route != path.end()) {
-    std::cout << *route << " ";
-    ++route;
-  }
-  std::cout << std::endl;
-  std::vector<position> POS;
-  dPQ.returnDijkstraPathPosition(start, end, POS);
-  std::vector<position>::iterator routePos = POS.begin();
+   mg.setInputCloud(*cloud_with_normal);
+   mg.findNearestProcess(selectedPoint);
+   std::cout << " 9. Merging completed" << std::endl;
+   std::vector<int> size3;
+   size3 = ft.getSegID();
+   std::cout << selectedPoint[0] << " " << selectedPoint[1] << " "
+   << size3.size() << std::endl;
+   std::cout << " 10. Delaunay3 function..." << std::endl;
+   std::vector<Triad> triads;
+   std::vector<Shx> ptsOut;
+   delaunay3 dy3;
+   dy3.setInputCloud(*cloud_with_normal);
+   dy3.putPointCloudIntoShx();
+   dy3.processDelaunay(triads);
+   dy3.getShx(ptsOut);
+   // write_Triads(triads, "triangles.txt");
+   // write_Shx(ptsOut, "pts.txt");
+   std::cout << " 10. Delaunay3 function completed..." << std::endl;
+   int start = selectedPoint[0];
+   int end = selectedPoint[1];
+   std::vector<int> path;
+   dijkstraPQ dPQ(size3.size());
+   dPQ.setInputCloud(*cloud_with_normal);
+   dPQ.setTri(triads);
+   dPQ.computeWeight();
+   dPQ.shortestPath(start, end);
+   dPQ.returnDijkstraPath(start, end, path);
+   res.path = path;
+   /*
+   std::vector<int>::iterator route = path.begin();
+   while (route != path.end()) {
+   std::cout << *route << " ";
+   ++route;
+   }
+   std::cout << std::endl;
+   std::vector<position> POS;
+   dPQ.returnDijkstraPathPosition(start, end, POS);
+   std::vector<position>::iterator routePos = POS.begin();
 
-  while (routePos != POS.end()) {
-    std::cout << (*routePos).x << " ";  // p.80
-    ++routePos;
-  }
-  std::cout << std::endl;
-  routePos = POS.begin();
-  while (routePos != POS.end()) {
-    std::cout << (*routePos).y << " ";
-    ++routePos;
-  }
-  std::cout << std::endl;
-  routePos = POS.begin();
-  while (routePos != POS.end()) {
-    std::cout << (*routePos).z << " ";
-    ++routePos;
-  }
+   while (routePos != POS.end()) {
+   std::cout << (*routePos).x << " ";  // p.80
+   ++routePos;
+   }
+   std::cout << std::endl;
+   routePos = POS.begin();
+   while (routePos != POS.end()) {
+   std::cout << (*routePos).y << " ";
+   ++routePos;
+   }
+   std::cout << std::endl;
+   routePos = POS.begin();
+   while (routePos != POS.end()) {
+   std::cout << (*routePos).z << " ";
+   ++routePos;
+   }
 
-  if (0) {
-    std::cout << " 11. Calculating weighting function..." << std::endl;
-    double** wei;
-    prepareDijkstra pD(size3);
-    pD.setInputCloud(*cloud_with_normal);
-    pD.setTri(triads);
-    pD.computeWeight();
-    pD.getWeight(wei);
-    std::cout << " 11. Calculation completed" << std::endl;
-    Graph Dpath(size3.size(), wei);
-    Dpath.initializeState();
-    Dpath.Dijkstra(start, end);
-    Dpath.returnDijkstraPath(start, end, path);
-    std::vector<int>::iterator route = path.begin();
-    std::cout << "Distance = " << Dpath.returnDijkstraPathDistance(end);
-    std::cout << std::endl;
-    while (route != path.end()) {
-      std::cout << *route << " ";
-      ++route;
-    }
+   if (0) {
+   std::cout << " 11. Calculating weighting function..." << std::endl;
+   double** wei;
+   prepareDijkstra pD(size3);
+   pD.setInputCloud(*cloud_with_normal);
+   pD.setTri(triads);
+   pD.computeWeight();
+   pD.getWeight(wei);
+   std::cout << " 11. Calculation completed" << std::endl;
+   Graph Dpath(size3.size(), wei);
+   Dpath.initializeState();
+   Dpath.Dijkstra(start, end);
+   Dpath.returnDijkstraPath(start, end, path);
+   std::vector<int>::iterator route = path.begin();
+   std::cout << "Distance = " << Dpath.returnDijkstraPathDistance(end);
+   std::cout << std::endl;
+   while (route != path.end()) {
+   std::cout << *route << " ";
+   ++route;
+   }
    }*/
 //---------------------------------------------------//
-
   return true;
 }
 
