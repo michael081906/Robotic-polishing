1bd1c8b06779bad84541989dcafb5fe0e38731b1
Michael Kam <31603609+michael081906@users.noreply.github.com>
2020-08-15 11:27:53 -0400

    Update dependencies_install.bash

--- dependencies_install.bash
+++ dependencies_install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone --recursive https://github.com/IFL-CAMP/iiwa_stack.git
-sudo apt install ros-kinetic-control-toolbox ros-kinetic-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-realtime-tool 
+sudo apt install ros-kinetic-control-toolbox ros-kinetic-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-realtime-tools 
 sudo apt-get install ros-kinetic-velocity-controllers ros-kinetic-ros-control ros-kinetic-position-controllers ros-kinetic-joint-state-controller ros-kinetic-joint-trajectory-controller
 #git clone --branch kinetic-devel --recursive https://github.com/ros-simulation/gazebo_ros_pkgs.git
 #git clone --branch kinetic-devel --recursive https://github.com/ros-controls/realtime_tools.git
